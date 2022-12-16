134e57d7b93b5301637da6e1530df717524cf9a5
Francisco Viana <newdesenvolvedor@gmail.com>
2022-12-16 18:28:02 -0300

    Criando diretorio e arquivos File_web

---
+++ Files_web/js/js.js
@@ -0,0 +1,19 @@
+<script>
+    
+    function typeWrite(elemento){
+        const textoArray = elemento.innerHTML.split('');
+        elemento.innerHTML = ' ';
+        textoArray.forEach(function(letra, i){   
+          
+        setTimeout(function(){
+            elemento.innerHTML += letra;
+        }, 85 * i)
+    
+      });
+}
+    const titulo = document.querySelector('.text');
+    typeWrite(titulo);
+</script>
+
+
+

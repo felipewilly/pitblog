834e896bee6669ed370a64293519b0ae3a858709
felipe willy <felipe00cj@gmail.com>
2023-09-29 08:43:21 -0300

    Configurado Docker para Produção Nginx

--- pitblog/pitblog/settings.py
+++ pitblog/pitblog/settings.py
@@ -110,12 +110,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'templates/static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'templates/static/'),
     ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -123,8 +123,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Arquivos de midia
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_ROOT = '/media/'
+MEDIA_URL = '/media/'
 
 
 MESSAGE_TAGS = {
